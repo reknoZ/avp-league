3dc06c53291d3f271d4cc743d31fb23ced2e4744
David Nitzsche-Bell <david@zicount.com>
2026-06-06 23:33:12 +0200

    Initial Commit

---
+++ AVP League/AVP_LeagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVP_LeagueApp.swift
+//  AVP League
+//
+//  Created by David on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct AVP_LeagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
